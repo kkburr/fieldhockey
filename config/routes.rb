4f0bacda9748d0d669fc66da25181598fa4938b1
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-10-31 14:53:51 -0600

    Cleans up routes.rb and adds sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'about' => 'static_pages#about'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :programs
 
   resources :players
 
   resources :users
-
-  root 'static_pages#home'
-
-  get 'about' => 'static_pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
