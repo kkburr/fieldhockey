32bf1399445c596972197729fc51946b4ea3f2b9
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-10-31 18:01:05 -0600

    Changes to users_controller:
    *Adds permissions for different actions.
    *Adds methods for logged_in_user (to view index),
    	correct_user (to edit user account), 
    	admin_user (to do all of the above & destroy)
    *Implements methods to save a URL and redirect you 
    	to it after sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page]).take(10)
   end
 
   # GET /users/1
@@ -44,8 +48,10 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      if @user.update_attributes(user_params)
+        format.html { 
+          flash[:sucess] = 'User was successfully updated.'
+          redirect_to root_path }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -57,9 +63,11 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    User.find(params[:id]).destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { 
+        flash[:success] = "User deleted"
+        redirect_to users_url  }
       format.json { head :no_content }
     end
   end
@@ -75,4 +83,25 @@ class UsersController < ApplicationController
       params.require(:user).permit(:first_name, :last_name, :email, :phone, :county, 
                                   :high_school, :password, :password_confirmation)
     end
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
