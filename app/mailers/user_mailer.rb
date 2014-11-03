55a9b1ed0bee3ae44e0f27e3829deca7a2653787
Kaitlin Barrer <kaitlin.barrer@gmail.com>
2014-11-02 20:08:57 -0700

    Changes default email for mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "kaitlin.barrer@gmail.com"
+  default from: "ColoradoFH@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
